d65d10e664f457a2b26e8b383034b29f572cb422
Aditya <adityat@accucia.co>
2022-01-08 18:06:30 +0530

    latest commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*
+class book for holding books data
+will act as a model class
+*/
 class Book {
     constructor(json) {
         this.name = json['name']
@@ -10,25 +14,25 @@ class Book {
     }
 }
 
+// Global variables
 let books = []
 let characters = []
 
+
+//Function to get a book from Ice and Fire Api and parse it into a book object
 async function getBookResponse(index) {
     var response = await fetch(`https://anapioficeandfire.com/api/books/${index}`)
     return (new Book(await response.json()))
 }
-
+//Function to get characters for a book and parse it
 async function getCharacterResponse(url) {
     var response = await fetch(url)
     return await response.json()
 }
 
+// Function used to get number of books, as per requirement 50 books are required
+// But the api only returned books till index 12 so repeating same function
 async function getBooks() {
-    for (let i = 1; i < 12; i++) {
-        let response = await getBookResponse(i);
-        books.push(response)
-        console.log(`Pushed Book ${response}`);
-    }
     for (let i = 1; i < 12; i++) {
         let response = await getBookResponse(i);
         books.push(response)
@@ -37,6 +41,7 @@ async function getBooks() {
     showBooks()
 }
 
+// Function to get characters list from api and push them into local array
 async function getCharacters(book) {
     characters.length = 0
     for (let i = 0; i < book.characters.length; i++) {
@@ -48,13 +53,15 @@ async function getCharacters(book) {
     }
 }
 
+
+//Function to show books in html by appending a card item to a div element
 let showBooks = () => {
     var rowElement = document.getElementById('row');
     books.forEach(book => {
         rowElement.innerHTML = rowElement.innerHTML + `
         <div class="col-md-3">
         <div class="card my-3" onclick="bookOnTap(${books.indexOf(book)})">
-            <img class="card-img-top" src="http://res.freestockphotos.biz/pictures/14/14345-illustration-of-an-open-book-pv.png" alt="">
+            <img class="card-img-top" src="book.png" alt="">
             <div class="card-body" >
                 <h4 class="card-title">${book.name}</h4>
                 <p class="card-text">ISBN No: ${book.isbn}</p>
@@ -69,14 +76,26 @@ let showBooks = () => {
 
 }
 
+// Function to handle the onclick event of the card
 let bookOnTap = async (index) => {
+
+    // Showing a loading indicator
+
+    // Created a local copy of current body
     var temp = document.body.innerHTML;
+    //Added the code for progress indicator and styled it to be in center
     document.body.innerHTML = `<div style = "position: absolute; top: 50%; right: 50%; transform: translateY(-50%, 50%)" class="spinner-border text-success">
     <span class="visually-hidden"></span>
 </div>`
+    //Getting data from the server
     let book = books[index];
     await getCharacters(book)
+    //Got data from the server
+
+    //Restoring body and hiding the spinner
     document.body.innerHTML = temp;
+
+    //Adding a pop up modal to display the characters list
     document.body.innerHTML += `
     <div id="myModal" class="modal fade" role="dialog">
         <div class="modal-dialog">
@@ -104,5 +123,5 @@ let bookOnTap = async (index) => {
 
 }
 
-getBooks()
-// alert('Click on particular book for their characters')
+getBooks().then(() => getBooks().then(() => getBooks()))
+alert('Click on particular book for their characters')
